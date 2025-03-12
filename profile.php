bc94b97f7fc9f07f6f9aa7c2e249d601f5c832e0
Thomas Jones <Thomasajones@hotmail.co.uk>
2025-03-12 12:47:58 +0000

    only remove friends commit

--- profile.php
+++ profile.php
@@ -1,9 +1,57 @@
 <?php include("includes/header.php");
+include ("classes/User.php");
+include ("classes/Post.php");
+
+if (isset($_GET['profile_username'])) {
+    $username = $_GET['profile_username'];
+
+    $username_details_query = mysqli_query($conn, "SELECT * FROM users WHERE username='$username'");
+    $user_array = mysqli_fetch_array($username_details_query);
+
+    $num_friends = (substr_count($user_array['friend_array'], ","))
+        - 1;
+}
 
 ?>
+<style>
+.wrapper {
+    margin-left: 0;
+    padding-left: 0;
+
+}
+</style>
+
+<div class="profile_left_section">
+    <img src="<?php echo $user_array['profile_pic'] ?>">
+
+    <div class="profile_information">
+        <p><?php echo "Posts: ". $user_array['num_posts']; ?></p>
+        <p><?php echo "Likes: ". $user_array['num_likes']; ?></p>
+        <p><?php echo "Friends: ". $num_friends; ?></p>
+    </div>
+
+    <form action="<?php echo $username ?>">
+        <?php
+        $profile_user_obj = new User($conn, $username);
+        if ($profile_user_obj->isClosed()) {
+            header("Location: user_closed.php");
+        }
+
+        $logged_in_user_obj = new User($conn, $userLoggedIn);
+
+        if($userLoggedIn != $username) {
+            if ($logged_in_user_obj->isFriend($username)) {
+                echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+
+            }
+        }
+        ?>
+    </form>
+
+</div>
 
 <div class="main_column column">
-    This is a profile page.
+    <?php echo $username?>
 </div>
 
 
---
+++ user_closed.php
@@ -0,0 +1,9 @@
+<?php
+include("includes/header.php");
+?>
+<div class="main_column column" id="main_column">
+<h4 style="font-size: xx-large; margin-top: 8px">User Closed</h4><br><br>
+
+<p style="font-size: medium">This account has been closed.</p>
+<a style="font-size: medium" href="index.php">Click here to go back</a>
+</div>
